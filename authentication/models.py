dfcb25d1661e44c5bccd164e88c86a1c8f09110b
Chreeeyons <cstuazon3@up.edu.ph>
2025-03-18 17:22:45 +0800

    add custom user model and viewsets for equipments and authentication and set up djangorestframework with corsheaders

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPES = [
+        ('student', 'Student'),
+        ('lab_technician', 'Lab Technician'),
+    ]
+    
+    user_type = models.CharField(max_length=20, choices=USER_TYPES, default='student')
+    
+    def save(self, *args, **kwargs):
+        if self.email:
+            self.username = self.email.split('@')[0]  # Set username from email
+        super().save(*args, **kwargs)
