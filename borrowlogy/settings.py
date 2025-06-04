8dd8595465fc0b7ee885d368f00d3c45f9d41cf5
Chreeeyons <cstuazon3@up.edu.ph>
2025-06-04 20:25:43 +0800

    Latest updates

--- borrowlogy/settings.py
+++ borrowlogy/settings.py
@@ -106,7 +106,7 @@ LOGIN_URLS = ('', 'auth/login',)
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_PUBLIC_URL'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=60,
         conn_health_checks=True,
     )
